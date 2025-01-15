813b3d31319a094ff98e4ebd4a1828f1f4a518d9
Honchar Denys <crackeraki@gmail.com>
2025-01-16 00:33:42 +0200

    added platforms

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'whub.webart.work',
+	appName: 'wHub',
+	webDir: 'dist/app/browser'
+};
+
+export default config;
